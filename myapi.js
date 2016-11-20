12a2c2f2e0d5499bc029d53e9eeb9b102067f885
Jeyabalaji <balaji.jeya@gmail.com>
2016-11-20 19:40:16 +0800

    one service created to glow LED

---
+++ myapi.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var express = require('express');
+var Gpio = require('onoff').Gpio;
+
+var app = express();
+
+var inputs = [{ pin: '11', gpio: '17', value: 1 },
+              { pin: '12', gpio: '18', value: 0 }];
+
+app.use(express['static'](__dirname ));
+
+app.get('/inputs/:id', function(req, res) {
+  console.log(req.params.id);
+  res.status(200).send(inputs[req.params.id]);
+});
+
+function glowLedFor5Sec(gpioPin) {
+  var led =  new Gpio(gpioPin, 'out');
+  led.writeSync(1);
+  setTimeout(function() {
+    led.writeSync(0);
+    led.unexport();
+  }, 5000);
+};
+
+app.post('/glowled/:pinNo', function(req, res) {
+  var output = {'error': 'Port not configured'};
+  if(req.params.pinNo === "17" || req.params.pinNo === "24") {
+    console.log("glowing LED");
+    output = {'success': true};
+    glowLedFor5Sec(req.params.pinNo);
+  }
+  res.status(200).send(output);
+});
+
+
+// Express route for any other unrecognised incoming requests
+app.get('*', function(req, res) {
+  res.status(404).send('Unrecognised API call');
+});
+
+// Express route to handle errors
+app.use(function(err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send('Oops, Something went wrong!');
+  } else {
+    next(err);
+  }
+});
+
+app.listen(3000);
+console.log('App Server running at port 3000');
+
