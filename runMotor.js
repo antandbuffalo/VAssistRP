1ada073011764c35fea73fbe457c72afb3de1d53
Jeyabalaji <balaji.jeya@gmail.com>
2016-11-27 20:38:54 +0800

    motor test program added

---
+++ runMotor.js
@@ -0,0 +1,16 @@
+var Gpio = require('onoff').Gpio;
+var input1 = new Gpio(23, 'out');
+var input2 = new Gpio(24, 'out');
+var enablePin = new Gpio(25, 'out');
+
+enablePin.writeSync(1);
+input1.writeSync(1);
+input2.writeSync(0);
+setTimeout(function() {
+  input1.writeSync(0);  // Turn LED off.
+  input1.unexport();    // Unexport GPIO and free resources
+  input2.writeSync(0);  // Turn LED off.
+  input2.unexport();    // Unexport GPIO and free resources
+  enablePin.writeSync(0);  // Turn LED off.
+  enablePin.unexport();    // Unexport GPIO and free resources
+}, 5000);
