79a56d5f76ec66b2df897f248b2455aab0611c50
Jeyabalaji <balaji.jeya@gmail.com>
2016-12-10 12:14:27 +0800

    order of input changed

--- runMotorBack.js
+++ runMotorBack.js
@@ -3,14 +3,14 @@ var input1 = new Gpio(23, 'out');
 var input2 = new Gpio(24, 'out');
 var enablePin = new Gpio(25, 'out');
 
-enablePin.writeSync(1);
 input1.writeSync(1);
 input2.writeSync(0);
+enablePin.writeSync(1);
 setTimeout(function() {
+  enablePin.writeSync(0);  // Turn LED off.
+  enablePin.unexport();    // Unexport GPIO and free resources
   input1.writeSync(0);  // Turn LED off.
   input1.unexport();    // Unexport GPIO and free resources
   input2.writeSync(0);  // Turn LED off.
   input2.unexport();    // Unexport GPIO and free resources
-  enablePin.writeSync(0);  // Turn LED off.
-  enablePin.unexport();    // Unexport GPIO and free resources
 }, 110);
